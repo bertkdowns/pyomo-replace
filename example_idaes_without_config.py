d0f4e29807b0c118901d7f66cc72ae6fcf597802
bd65 <bd65@students.waikato.ac.nz>
2025-10-07 12:36:12 +1300

    nice example of usage

--- example_idaes_without_config.py
+++ example_idaes_without_config.py
@@ -1,6 +1,6 @@
 from model import *
 import pyomo.environ as pyo
-from unit_models.heater import DutyHeater
+from idaes.models.unit_models import Heater
 from idaes.core import FlowsheetBlock
 from idaes.models.properties import iapws95
 
@@ -8,18 +8,24 @@ from idaes.models.properties import iapws95
 m = pyo.ConcreteModel()
 m.fs = FlowsheetBlock(dynamic=False)
 m.fs.pp = iapws95.Iapws95ParameterBlock()
-m.fs.h1 = DutyHeater(property_package=m.fs.pp, has_pressure_change=True)
-
-register_inlet_ports(m.fs)
-
+m.fs.h1 = Heater(property_package=m.fs.pp, has_pressure_change=True)
 m.fs.h1.inlet.flow_mol.fix(1)
 m.fs.h1.inlet.enth_mol.fix(3000)
 m.fs.h1.inlet.pressure.fix(1e5)
 
+register_block(
+    m.fs.h1,
+    [
+        m.fs.h1.inlet.flow_mol,
+        m.fs.h1.inlet.enth_mol,
+        m.fs.h1.inlet.pressure,
+        m.fs.h1.heat_duty,
+        m.fs.h1.deltaP,
+    ],
+)
+
 replace_state_var(m.fs.h1.heat_duty, m.fs.h1.outlet.enth_mol)
 replace_state_var(m.fs.h1.inlet.pressure, m.fs.h1.outlet.pressure)
 
-m.fs.h1.outlet.enth_mol.fix(3500)
-m.fs.h1.outlet.pressure.fix(1.1e5)
 
 pprint_replacements(m.fs)
